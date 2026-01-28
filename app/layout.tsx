7b616d09ec93b476d2cff09fb5163619c23813ed
Martin Graw <jmscalco@gmail.com>
2026-01-28 11:13:58 -0800

    Fix Font Awesome loading with CDN

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter, JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 import { TradeProvider } from './context/TradeContext';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -30,6 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${jetbrainsMono.variable}`}>
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
       <body>
         <TradeProvider>{children}</TradeProvider>
       </body>
